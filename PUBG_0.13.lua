b22e8a71d96900eb220df05f1a7f4f4362242814
Trương Minh Quý <truongminhquy46@gmail.com>
2019-07-25 20:13:57 +0700

    Add files via upload

---
+++ PUBG_0.13.lua
@@ -0,0 +1,279 @@
+_T = "🔎 𝗪𝗔𝗟𝗟 𝗛𝗔𝗖𝗞 『 𝗙𝗜𝗡𝗗 𝗩𝗔𝗟𝗨𝗘 』"
+_T_ = '🔯 𝗖𝗢𝗟𝗢𝗥 『 𝗙𝗜𝗡𝗗 𝗩𝗔𝗟𝗨𝗘 』'
+X = {_T, _T_, '〽️ 𝗔𝗡𝗧𝗜 𝗦𝗛𝗔𝗞𝗘 『 𝗜𝗡𝗚𝗔𝗠𝗘 』', '🧬 𝗔𝗡𝗧𝗘𝗡𝗔 𝗣𝗥𝗢 『 𝗜𝗡𝗚𝗔𝗠𝗘 』', '🔰 𝗔𝗨𝗧𝗢 𝗛𝗘𝗔𝗗𝗦𝗛𝗢𝗧 『 𝗜𝗡𝗚𝗔𝗠𝗘 』', '🚫 𝗟𝗘𝗦𝗦 𝗥𝗘𝗖𝗢𝗜𝗟 『 𝗟𝗢𝗕𝗕𝗬 』', '🤺 𝗔𝗡𝗧𝗘𝗡𝗔 𝗚𝗛𝗜𝗟𝗟𝗜𝗘 『 𝗜𝗡𝗚𝗔𝗠𝗘 』', '🌱 𝗡𝗢 𝗚𝗥𝗔𝗦𝗦 𝗘𝗥𝗔𝗡𝗚𝗘𝗟 『 𝗜𝗡𝗚𝗔𝗠𝗘 』', '⛔   ᴇxιт   ⛔', nil, '🛡 vιᴘ нᴀcκ ᴘuʙԍ 0.12 ʙʏ мм2512 🛡', gg.choice, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, -1, gg.REGION_ANONYMOUS, gg.REGION_C_ALLOC, gg.TYPE_DWORD, gg.TYPE_FLOAT, gg.clearResults, gg.setRanges, gg.searchNumber, gg.getResults, false, gg.SIGN_EQUAL, gg.editAll, gg.toast, true, gg.getResultCount, gg.addListItems, gg.alert, gg.setVisible, gg.loadResults, gg.isVisible, gg.searchAddress, ipairs, gg.clearList, string.format, gg.skipRestoreState, os.exit, '✔𝗔𝗡𝗧𝗜 𝗦𝗛𝗔𝗞𝗘 ✔', '✔𝗔𝗡𝗧𝗘𝗡𝗔 𝗣𝗥𝗢 ✔', '✔𝗔𝗨𝗧𝗢 𝗛𝗘𝗔𝗗𝗦𝗛𝗢𝗧 ✔', '✔ 𝗟𝗘𝗦𝗦 𝗥𝗘𝗖𝗢𝗜𝗟 ✔', '✔𝗔𝗡𝗧𝗘𝗡𝗔 𝗚𝗛𝗜𝗟𝗟𝗜𝗘 ✔', '✔ 𝗡𝗢 𝗚𝗥𝗔𝗦𝗦 𝗘𝗥𝗔𝗡𝗚𝗘𝗟 ✔', '🅴🆁🆁🅾🆁\nvᴀʟuᴇ ɴoт ғouɴᴅ', '🔯 Edit ᴡᴀʟʟ ʜᴀᴄᴋ vᴀʟuᴇ ', '🇻🇳 ʙʏ мм2512 🇻🇳', '♻ ʀᴇᴅ [ᴅᴇғᴀᴜʟᴛ]', '♻ ʀᴇᴅ ғʟᴀsʜ', '♻ ᴅᴀʀᴋ ʀᴇᴅ', '♻ ᴅᴀʀᴋ ɢʀᴇᴇɴ', '♻ ᴅᴀʀᴋ ᴘɪɴᴋ', '♻ ᴅᴀʀᴋ ʙʟᴜᴇ', '♻ ʙʟᴀᴄᴋ', '♻ ʏᴇʟʟᴏᴡ ᴍɪx ɢʀᴇᴇɴ', '♻ ᴡʜɪᴛᴇ ᴍɪx ᴘɪɴᴋ', '♻ ᴡʜɪᴛᴇ ᴍɪx ᴠ2', '♻ ᴡʜɪᴛᴇ ᴍɪx ᴠ3', '♻ ᴍɪʟᴋ ᴍɪx', '🔙 ʙᴀᴄᴋ 🔙', '🇻🇳 ᴄʜᴏᴏsᴇ ᴄᴏʟᴏʀ ʏᴏᴜ ᴡᴀɴᴛ 🇻🇳', '♻ 𝗖𝗢𝗟𝗢𝗥 『 𝗙𝗜𝗫 𝗩𝗔𝗟𝗨𝗘 』', '🧰 Edit 𝗖𝗢𝗟𝗢𝗥 vᴀʟuᴇ ', '⚙ 𝗪𝗔𝗟𝗟 𝗛𝗔𝗖𝗞 『 𝗙𝗜𝗫 𝗩𝗔𝗟𝗨𝗘 』', 0, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90}
+function xxx(x, xx)
+X[46](x, 0xFFFFFFFF, X[29])
+X[34](1)
+X[37](''.. xx, X[29])
+X[31]()
+end
+
+function xx()
+
+xxxx = X[12]({X[5], X[3], X[4], X[7], X[8], X[6], X[1], X[2], '✨ Wallhack SD845', '🎨 Colour SD845 Red', '🎨 Colour SD845 Yellow', '🎨 Colour SD845 Red HDR', X[9]}, X[10], X[11])
+
+if xxxx == X[13] then xxxxx() X[38](X[54]) end
+if xxxx == X[14] then xxxxxx() X[38](X[52]) end
+if xxxx == X[15] then xxxxxxx() X[38](X[53]) end
+if xxxx == X[16] then xxxxxxxx() X[38](X[56]) end
+if xxxx == X[17] then xxxxxxxxx() X[38](X[57]) end
+if xxxx == X[18] then xxxxxxxxxx() X[38](X[55]) end
+if xxxx == X[19] then xxxxxxxxxxx() end
+if xxxx == X[20] then xxxxxxxxxxxx() end
+if xxxx == 9 then WH845() end
+if xxxx == 10 then RED845() end
+if xxxx == 11 then YLW845() end
+if xxxx == 12 then REDHDR845() end
+if xxxx == 13 then xxxxxxxxxxxxxxxxx() end
+xxxxxxxxxxxxxxxx = X[26]
+end
+function WH845()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('95D;2;9.2194229e-41::100', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('2', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(6)
+gg.editAll('120', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('5.201992e21;2.25000452995;2;1.6623054e-19', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('2', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(4)
+gg.editAll('120', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.searchNumber('2;-1;0;1;-127;0.24022650719;0.69314718246;0.00999999978::30', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('2', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll('120', gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('Wallhack SD845 activated!')
+end
+function RED845()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('1,661,566,999;96;539,246,604;8200::', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('8200', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(10)
+gg.editAll('7', gg.TYPE_DWORD)
+gg.toast('Colour Red SD845 activated!')
+end
+function YLW845()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('536887312;539246610;-2128609280;8200', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('8200', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(20)
+gg.editAll('6', gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast('Colour Yellow SD845 activated!')
+end
+function REDHDR845()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('256;8200;26', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('8200', gg.TYPE_DWORD,false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(5)
+gg.editAll('7', gg.TYPE_DWORD)
+gg.toast('Colour Red HDR SD845 activated!')
+gg.clearResults()
+end
+function xxxxxx()
+X[31]()
+X[32](X[27])
+X[33]("0.20000000298~0.30000001192F;53.0F;30.0F;1.0F::512", X[30], X[35], X[36], 0, X[26])
+X[33]("0.20000000298~0.30000001192F;1.0F::512", X[30], X[35], X[36],0, X[26])
+X[34](200)
+X[37]("1.4012985e-45", X[30])
+X[31]()
+  end
+function xxxxxxx()
+X[31]()
+X[32](X[27])
+X[33]("88.50576019287F;87.27782440186F;X[26]00.91194152832F;1F::13", X[30], X[35], X[36], 0, X[26])
+X[33]("88.50576019287F;87.27782440186F;1F", X[30], X[35], X[36], 0, X[26])
+X[34](X[18])
+X[37]("1.96875;1.96875;999;1.96875;1.96875;999", X[30])
+X[31]()
+end
+
+function xxxxxxxx()
+X[31]()
+X[32](X[27])
+X[33]("0.98900693655~0.98900723457;0.14786802232~0.14786840975;1.1920926e-7::9", X[30], X[35], X[36], 0, X[26])
+X[33]("0.98900693655~0.98900723457", X[30], X[35], X[36], 0, X[26])
+X[34](100)
+X[37]("16000", X[30])
+X[31]()
+end
+
+function xxxxx()
+X[32](X[27])
+X[33]("9.20161819458;23;25;30.5", X[30], X[35], X[36], 0, X[26])
+X[33]("25;30.5", X[30], X[35], X[36], 0, X[26])
+X[34](10)
+X[37]("240", X[30])
+X[31]()
+end
+
+function xxxxxxxxxx()
+X[31]()
+X[32](X[27])
+X[33]("1.5584387e28", X[30], X[35], X[36],0, X[26])
+X[34](100)
+X[37]("0", X[30])
+X[31]()
+X[32](X[27])
+X[33]("1D;0.05000000075F;0.10000000149F;0.55000001192F;9.5F;15.0F", X[30], X[35], X[36],0, X[26])
+X[33]("1", X[29], X[35], X[36],0, X[26])
+X[34](800)
+X[37]("0", X[29])
+X[31]()
+X[31]()
+end
+
+
+function xxxxxxxxx()
+X[31]()
+X[32](X[27])
+X[33]('8.0F;1.20000004768F;0.80000001192F;1.5F;0.80000001192F;1.5F:512', X[30], X[35], X[36], 0, X[26])
+X[33]('8', X[30], X[35], X[36], 0, X[26])
+X[34](100)
+X[37]('0', X[30])
+X[31]()
+end
+
+_FORV_2_ = X[78]
+function xxxxxxxxxxx()
+if _FORV_2_ == X[13] then xxxxxxxxxxxxxxx()
+else
+X[48]()
+X[31]()
+X[32](X[28])
+X[33]("0.5;2.8025969e-45", X[30], X[35], X[36], 0, X[26])
+X[33]("0.5", X[30], X[35], X[36], 0, X[26])
+X[34](100)
+X[37]("0", X[30])
+X[31]()
+X[33]("0.5;2.8025969e-45;4D;2D;14D;5D;-1D", X[30], X[35], X[36], 0, X[26])
+if X[40]() == X[78] then X[42](X[58])
+else
+X[33]("0.5", X[30], X[35], X[36], 0, X[26])
+_FORV_4_ = X[40]()
+_FORV_6_ = X[34](_FORV_4_)
+_FORV_5_ = _FORV_4_ +1
+_FORV_2_ = X[13]
+_T = X[77]
+xxxxxxxxxxxxxxx()
+end
+end
+end
+function xxxxxxxxxxxxxxx()
+if _FORV_2_ == X[78] then X[42](X[58])
+else
+X[43](X[35]) _FORV_3_ = {}
+for _FORV_9_ = X[13], _FORV_4_ do
+_FORV_3_[_FORV_9_] = X[59] .. _FORV_9_.. ''
+end
+_FORV_3_[_FORV_5_] = X[73]
+_FORV_7_ = X[12](_FORV_3_, X[10], X[60])
+for _FORV_11_ = X[13], _FORV_4_ do
+if _FORV_7_ == _FORV_11_ then
+for _FORV_9_, _FORV_10_ in X[47] (_FORV_6_) do
+_FORV_6_[_FORV_9_].freeze = X[35]
+X[41](_FORV_6_)
+end
+_FORV_6_[_FORV_11_].value = X[13]
+_FORV_6_[_FORV_11_].freeze = X[39]
+X[48]()
+_FORV_8_ = {}
+_FORV_8_[X[13]] = {}
+_FORV_8_[X[13]].address = _FORV_6_[_FORV_11_].address
+_FORV_8_[X[13]].flags = X[30]
+_FORV_8_[X[13]].value = _FORV_6_[_FORV_11_].value
+_FORV_8_[X[13]].freeze = X[39]
+X[41](_FORV_8_)
+k = _FORV_4_
+else
+if _FORV_12_ == _FORV_4_+1 then xx() end
+end
+end
+end
+X[43](X[39])
+end
+_FORV_=X[78]
+_FORV_0_ = X[97]
+function xxxxxxxxxxxx()
+if _FORV_ == X[13] then xxxxxxxxxxxxx()
+elseif _FORV_ == X[14] then xxxxxxxxxxxxxx()
+else
+_FORV_1_ = X[12]({X[61], X[62], X[63], X[64], X[65], X[66], X[67], X[68], X[69], X[70], X[71], X[72], X[73]}, X[10], X[74])
+if _FORV_1_ == X[13] then _FORV_=X[13] _FORV_0_=X[97] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[14] then _FORV_=X[13] _FORV_0_=X[84] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[15] then _FORV_=X[13] _FORV_0_=X[96] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[16] then _FORV_=X[13] _FORV_0_=X[94] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[17] then _FORV_=X[13] _FORV_0_=X[92] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[18] then _FORV_=X[13] _FORV_0_=X[88] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[19] then _FORV_=X[13] _FORV_0_=X[146] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[20] then _FORV_=X[13] _FORV_0_=X[95] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[21] then _FORV_=X[13] _FORV_0_=X[137] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[22] then _FORV_=X[13] _FORV_0_=X[90] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[23] then _FORV_=X[13] _FORV_0_=X[79] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[24] then _FORV_=X[13] _FORV_0_=X[87] xxxxxxxxxxxxx() end
+if _FORV_1_ == X[25] then xx() end
+xxxxxxxxxxxxxxxx = X[26]
+end
+end
+function xxxxxxxxxxxxx()
+X[31]()
+X[32](X[28])
+X[33]("32;16;40;48;2:45", X[29], X[35], X[36], 0, X[26])
+if X[40]() == X[78] then
+X[42](X[58])
+else
+_FORV_ = X[13]
+X[33]("40", X[29], X[35], X[36], 0, X[26])
+_FORV_13_ = X[40]()
+_FORV_15_ = X[34](_FORV_13_)
+_FORV_14_ = _FORV_13_ +1
+_FORV_ = X[14]
+xxxxxxxxxxxxxx()
+end
+end
+function xxxxxxxxxxxxxx()
+_T_ = X[75]
+_FORV_16_ = {}
+for _FORV_9_, _FORV_10_ in X[47](_FORV_15_) do
+_FORV_16_[_FORV_9_] = {}
+_FORV_16_[_FORV_9_] = X[49]('%08X', _FORV_15_[_FORV_9_].address):sub(-8)
+end
+__FORV_16__ = {}
+for _FORV_9_ = X[13], _FORV_13_ do
+__FORV_16__[_FORV_9_] = X[76] .. _FORV_9_ .. ''
+end
+__FORV_16__[_FORV_14_] = X[73]
+_FORV_17_ = X[12](__FORV_16__, X[10], X[60])
+for _FORV_19_ = X[13], _FORV_13_ do
+if _FORV_17_ == _FORV_19_ then
+for _FORV_9_, _FORV_10_ in X[47] (_FORV_15_) do
+xxx(_FORV_16_[_FORV_9_], X[99])
+end
+xxx(_FORV_16_[_FORV_19_], _FORV_0_)
+_FORV_19_ = _FORV_13_
+else
+if _FORV_18_ == _FORV_14_ then xx() end
+end
+end
+end
+
+function xxxxxxxxxxxxxxxxx()
+X[50]()
+X[51]()
+X[43](X[39])
+end
+
+while X[39] do
+if X[45](X[39]) then
+xxxxxxxxxxxxxxxx = X[13]
+X[43](X[35])
+end
+X[31]()
+if xxxxxxxxxxxxxxxx == X[13] then xx() end
+end
